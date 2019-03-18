c9d55db60e55981587648b2c7b1bb5fd4a131466
Adrian-George Bostan <adrg@epistack.com>
2019-03-18 12:33:22 +0200

    Make HTTP errors public

--- http.go
+++ http.go
@@ -2,24 +2,24 @@ package errors
 
 import "fmt"
 
-type http struct {
+type HTTP struct {
 	*primitive
 	code int
 }
 
-func (h http) Code() int {
+func (h HTTP) Code() int {
 	return h.code
 }
 
-func HTTP(cause error, code int, message string) error {
-	return &http{
+func NewHTTP(cause error, code int, message string) error {
+	return &HTTP{
 		primitive: newPrimitive(cause, message),
 		code:      code,
 	}
 }
 
 func HTTPf(cause error, code int, format string, args ...interface{}) error {
-	return &http{
+	return &HTTP{
 		primitive: newPrimitive(cause, fmt.Sprintf(format, args...)),
 		code:      code,
 	}
